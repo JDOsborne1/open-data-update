16561542d8c1a639fa240549a5e1ef626deda60b
Matthew Burgess <matthew.burgess@edit.co.uk>
2019-07-02 14:10:39 +0100

    Changes

--- land_registry_data.R
+++ land_registry_data.R
@@ -1,24 +1,38 @@
 library(SPARQL)
-library(ggplot2)
 
 # Define the data.gov endpoint
 endpoint <- "http://landregistry.data.gov.uk/landregistry/query"
 
 query <-
-  "PREFIX lrppi: <http://landregistry.data.gov.uk/def/ppi/>
-  SELECT ?postcode ?amount ?date
+  "
+  prefix lrppi: <http://landregistry.data.gov.uk/def/ppi/>
+  prefix lrcommon: <http://landregistry.data.gov.uk/def/common/>
+  prefix skos: <http://www.w3.org/2004/02/skos/core#>
+  prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  SELECT *
     WHERE {
-     ?transx a <http://landregistry.data.gov.uk/def/ppi/TransactionRecord> .
-     ?transx <http://landregistry.data.gov.uk/def/ppi/pricePaid> ?amount .
-     ?transx <http://landregistry.data.gov.uk/def/ppi/transactionDate> ?date .
-     ?transx <http://landregistry.data.gov.uk/def/ppi/propertyAddress> ?addr.
-     ?transx <http://landregistry.data.gov.uk/def/ppi/recordStatus> ?status.
-     ?addr <http://landregistry.data.gov.uk/def/common/postcode> ?postcode .
+     ?transx lrppi:transactionId ?trans_id .
+     ?transx lrppi:pricePaid ?amount .
+     ?transx lrppi:transactionDate ?date .
+     ?transx lrppi:propertyAddress ?addr .
+     ?transx lrppi:recordStatus/skos:prefLabel ?Record_status .
+     ?transx lrppi:transactionCategory/rdfs:label ?transaction_category .
+     ?transx lrppi:estateType/skos:prefLabel ?estate_type .
+     ?transx lrppi:propertyType/skos:prefLabel ?property_type .
+     ?transx lrppi:newBuild ?new_build .
+     ?addr lrcommon:postcode ?postcode .
+     OPTIONAL {?addr lrcommon:county ?county}
+     OPTIONAL {?addr lrcommon:paon ?paon}
+     OPTIONAL {?addr lrcommon:saon ?saon}
+     OPTIONAL {?addr lrcommon:street ?street}
+     OPTIONAL {?addr lrcommon:town ?town}
     }
   LIMIT 10"
 
 # Step 2 - Use SPARQL package to submit query and save results to a data frame
-qd <- SPARQL(endpoint, query)
+qd <- SPARQL(endpoint, query)$results
+qd
+
 df <- qd$results
 
 as.POSIXct(1417737600, origin = "1970-01-01")
