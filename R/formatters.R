775b43270ac8690c351afc64aa450d44652f3588
Joseph Osborne <j_osborne@edit.co.uk>
2019-07-29 16:51:55 +0100

    Package structure commit

---
+++ R/formatters.R
@@ -0,0 +1,17 @@
+#' Clean date fields
+#'
+#' @param field 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cleanFieldType <- function(field){
+        output <- stringr::str_match(field, '^"?([\\w-]+)"?')[, 2]
+        return(output)
+} 
+
+cleanRegionName <- function(field){
+        output <- stringr::str_match(field, '.*region/(\\S+)>$')[, 2]
+        return(output)
+        }
---
+++ R/getters.R
@@ -0,0 +1,103 @@
+# Functions to get the various sources
+
+
+
+#' getSalesData
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getSales <- function(Limited = TRUE){
+                
+        limiter_string <- if_else(Limited, "LIMIT 10", "")
+                
+        query <- paste0(
+                        "
+                        prefix lrppi: <http://landregistry.data.gov.uk/def/ppi/>
+                        prefix lrcommon: <http://landregistry.data.gov.uk/def/common/>
+                        prefix skos: <http://www.w3.org/2004/02/skos/core#>
+                        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+                        SELECT ?trans_id_str ?price_paid ?date ?postcode ?property_type ?new_build ?estate_type ?saon ?paon ?street ?locality ?town ?district ?county ?transaction_category ?Record_status
+                        WHERE {
+                        ?transx lrppi:transactionId ?trans_id .
+                        ?transx lrppi:pricePaid ?price_paid .
+                        ?transx lrppi:transactionDate ?date .
+                        ?transx lrppi:propertyAddress ?addr .
+                        ?transx lrppi:recordStatus/skos:prefLabel ?Record_status .
+                        ?transx lrppi:transactionCategory/rdfs:label ?transaction_category .
+                        ?transx lrppi:estateType/skos:prefLabel ?estate_type .
+                        ?transx lrppi:propertyType/skos:prefLabel ?property_type .
+                        ?transx lrppi:newBuild ?new_build .
+                        ?addr lrcommon:postcode ?postcode .
+                        OPTIONAL {?addr lrcommon:county ?county}
+                        OPTIONAL {?addr lrcommon:paon ?paon}
+                        OPTIONAL {?addr lrcommon:saon ?saon}
+                        OPTIONAL {?addr lrcommon:street ?street}
+                        OPTIONAL {?addr lrcommon:locality ?locality}
+                        OPTIONAL {?addr lrcommon:district ?district}
+                        OPTIONAL {?addr lrcommon:town ?town}
+                        bind( str(?trans_id) as ?trans_idr_str )
+                        }
+                        "
+        , limiter_string
+        )
+
+        price_paid <- landRegistryQuery(query)
+        
+        price_paid <- price_paid %>%
+                mutate_at(vars(property_type, estate_type, transaction_category, Record_status), cleanFieldType) %>%
+                mutate(date = as.POSIXct(date, origin = "1970-01-01"))
+        
+        return(price_paid)
+}
+
+
+#' House Price Index Getter
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getHPI <- function(Limited = TRUE){
+        
+        limiter_string <- if_else(Limited, "LIMIT 10", "")
+        
+        query <- paste0(
+        '
+        prefix lrppi: <http://landregistry.data.gov.uk/def/ppi/>
+        prefix lrcommon: <http://landregistry.data.gov.uk/def/common/>
+        prefix skos: <http://www.w3.org/2004/02/skos/core#>
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        prefix ukhpi: <http://landregistry.data.gov.uk/def/ukhpi/>
+        prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+
+        SELECT ?region ?date ?hpi ?regionname 
+        {
+                ?region ukhpi:refPeriodStart ?date ;
+                        ukhpi:housePriceIndex ?hpi ;
+                        ukhpi:refRegion ?regionname
+        FILTER (
+                ?date > "2019-04-01"^^xsd:date 
+        )
+        }
+        '
+        , limiter_string
+        )
+        
+        
+        hpi <- landRegistryQuery(query)
+        
+        hpi <- hpi %>%
+                mutate(date = as.POSIXct(date, origin = "1970-01-01")) %>% 
+                mutate(regionname = cleanRegionName(regionname))
+        
+        return(hpi)
+}
+
+landRegistryQuery <- function(Query, Endpoint = "http://landregistry.data.gov.uk/landregistry/query"){
+        returnedObj <- SPARQL::SPARQL(Endpoint, Query)
+        output <- returnedObj$results
+        return(output)
+}
